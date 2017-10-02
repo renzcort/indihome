8fffb7445d3b8549a60812ebe6a974cfa45fdf31
renzcort <kharenputra@gmail.com>
2017-10-02 16:34:50 +0700

    Add conversion tags

--- index.php
+++ index.php
@@ -287,6 +287,25 @@
 	  </div><!-- /.modal-dialog -->
 	</div><!-- /.modal -->
 
+	<!-- Google Code for IndiHome Conversion Conversion Page -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = 842107730;
+	var google_conversion_language = "en";
+	var google_conversion_format = "3";
+	var google_conversion_color = "ffffff";
+	var google_conversion_label = "-fpfCKyO5HMQ0pbGkQM";
+	var google_remarketing_only = false;
+	/* ]]> */
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+	<div style="display:inline;">
+		<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/842107730/?label=-fpfCKyO5HMQ0pbGkQM&amp;guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
+	
 	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/index.js"></script>
